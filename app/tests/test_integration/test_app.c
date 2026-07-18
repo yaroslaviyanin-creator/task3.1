5a50ac8a368ca6d57ff850af5cd2b6d350a12b19
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-18 10:21:47 +0500

    Интеграционные тесты обработчика файлов

--- app/tests/test_integration/test_app.c
+++ app/tests/test_integration/test_app.c
@@ -5,10 +5,26 @@ test_app.c - модуль проверки программы.
 */
 
 #include "unity.h"
-
 #include "lib_main.h"
 
-int main(void) {
-    return 0;
+void setUp(void) {}
+void tearDown(void) {}
+
+// Передача несуществующего файла должна возвращать код ошибки 1
+void test_process_file_invalid_input(void) {
+    TArg arg_prog;
+    arg_prog.input_file = "non_existent_file_12345.txt";
+    arg_prog.output_file = "out.txt";
+    arg_prog.type_proc = "sort";
+    arg_prog.type_sort = "fast";
+    arg_prog.type_cmp = 1;
+
+    int res = process_file(arg_prog);
+    TEST_ASSERT_EQUAL_INT(1, res);
 }
 
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_process_file_invalid_input);
+    return UNITY_END();
+}
